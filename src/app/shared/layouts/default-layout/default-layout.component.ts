e28fe0400f498b215c04719de87a5a406380c8d8
waleedo0o7 <waleedhd1@gmail.com>
2023-10-22 00:57:44 +0300

    force logout if user not authorized

--- src/app/shared/layouts/default-layout/default-layout.component.ts
+++ src/app/shared/layouts/default-layout/default-layout.component.ts
@@ -1,10 +1,19 @@
+import { SharedService } from 'src/app/shared/shared.service';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-default-layout',
   templateUrl: './default-layout.component.html',
-  styleUrls: ['./default-layout.component.scss']
+  styleUrls: ['./default-layout.component.scss'],
 })
 export class DefaultLayoutComponent {
 
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    if (!localStorage.getItem('authToken')) {
+      this.router.navigate(['/']);
+    }
+  }
 }
